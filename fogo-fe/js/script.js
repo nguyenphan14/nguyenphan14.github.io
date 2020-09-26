c5a3c34f91bd1476a7f34b4ff09149280852b866
phannguyen14 <chiahet2001@gmail.com>
2020-09-26 15:30:11 +0700

    Checkout dev branch & single page index.html

--- fogo-fe/js/script.js
+++ fogo-fe/js/script.js
@@ -1,9 +1,17 @@
+const homePage = document.getElementById('home-page');
+const roomPage = document.getElementById('room-page');
 const roomList = document.querySelector('#room-card');
+const homeLink = document.getElementById('home-link');
+
 const seenRooom = [];
 for(let i = 0; i < 5; i++) {
     renderRoom(roomData.data[i]);
 }
 addAllBtn();
+
+homeLink.addEventListener('click', () => {
+    roomToHome();
+})
 
 document.querySelector('.all-btn').addEventListener('click', (event) => {
     for(let i = 5; i < roomData.data.length; i++) {
@@ -22,10 +30,16 @@ function addAllBtn() {
     roomList.append(div);
 }
 
+
 function getDetail() {
     let detail = this.id;
     seenRooom.push(detail);
     sessionStorage.setItem('roomId', seenRooom);
+    renderDetail(data[0]);
+    renderImg(data[0]);
+    renderHost(data[0]);
+    renderNote(data[0]);
+    homeToRoom();
 }
 
 function renderRoom(room) {
@@ -34,8 +48,6 @@ function renderRoom(room) {
     div.className = 'border-top my-3';
     a.className = 'row room-preview';
     a.id = room.id;
-    a.href = "room.html";
-    a.addEventListener('click', getDetail);
     a.innerHTML = `
         <div class="col-md-4">
         <img class="room-thumbnail" src="${room.image}">
@@ -72,4 +84,138 @@ function renderRoom(room) {
     `
     roomList.append(a);
     roomList.append(div);
+    a.addEventListener('click', getDetail);
+}
+
+/* Render detail */
+
+let roomId = sessionStorage.getItem('roomId').split(",");
+let detail = roomId[roomId.length-1];
+const containerImg = document.getElementById('container-img');
+let data = details.filter((room) => {
+    return room.id === detail;
+})
+/* renderDetail(data[0]);
+renderImg(data[0]);
+renderHost(data[0]);
+renderNote(data[0]); */
+function renderDetail(room) {
+    document.querySelector('#room-price > span').textContent = `${room.details.price.value*1000000}`;
+    document.querySelector('#room-area > span').textContent = `${room.details.area} m²`;
+    document.querySelector('#room-deposit > span').textContent = `${room.details.deposit}`;
+    document.querySelector('#room-capacity > span').textContent = `${room.details.capacity} người`;
+    document.querySelector('#room-electric > span').textContent = `${room.details.additionalFee.electric.value}k\\${room.details.additionalFee.electric.unit}`;
+    document.querySelector('#room-water > span').textContent = `${room.details.additionalFee.water.value}k\\${room.details.additionalFee.water.unit}`;
+    document.querySelector('#room-other_fee > span').textContent = `${room.details.additionalFee.other}`;
+    document.querySelector('#room-address > span').textContent = `Đường ${room.address.street}, phường ${room.address.ward.text} - ${room.address.district.text} - ${room.address.city.text}`;
+    if(room.utils.airConditioner === false) {
+        document.querySelector('#room-air_conditioner').style.display = 'none';
+    }
+    if(room.utils.bathroom === false) {
+        document.querySelector('#room-bathroom').style.display = 'none';
+    }
+    if(room.utils.parkingArea === false) {
+        document.querySelector('#room-parking_area').style.display = 'none';
+    }
+    if(room.utils.wifi === false) {
+        document.querySelector('#room-wifi').style.display = 'none';
+    }
+    if(room.utils.liveWithOwner === false) {
+        document.querySelector('#room-owner_living').style.display = 'none';
+    }
+    if(room.utils.fridge === false) {
+        document.querySelector('#room-fridge').style.display = 'none';
+    }
+    if(room.utils.washingMachine === false) {
+        document.querySelector('#room-washing_machine').style.display = 'none';
+    }
+    if(room.utils.waterHeater === false) {
+        document.querySelector('#room-water_heater').style.display = 'none';
+    }
+    if(room.utils.bed === false) {
+        document.querySelector('#room-bed').style.display = 'none';
+    }
+    if(room.utils.closet === false) {
+        document.querySelector('#room-closet').style.display = 'none';
+    }
+    if(room.utils.petsAllowed === false) {
+        document.querySelector('#pet_allowed').style.display = 'none';
+    }
+    if(room.utils.garret === false) {
+        document.querySelector('#room-garret').style.display = 'none';
+    }
+    if(room.utils.window === false) {
+        document.querySelector('#room-window').style.display = 'none';
+    }
+    if(room.utils.balcony === false) {
+        document.querySelector('#room-balcony').style.display = 'none';
+    }
+    if(room.utils.cookingAllowed === false) {
+        document.querySelector('#room-cooking_allowed').style.display = 'none';
+    }
+    
+}
+
+
+/* Images modal */
+
+function renderModal(event) {
+    let modal = document.querySelector(`.${event.target.id}`);
+    modal.style.display = 'block';
+    console.log(modal);
+    let closeBtns = document.getElementsByClassName('close-btn');
+    for(let btn of closeBtns) {
+        btn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        })
+    }
+}
+
+/* render img */
+
+function renderImg(room) {
+    for(let i = 0; i < room.images.length; i++) {
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <img id="img-${i}"class="img-fluid p-1 room-img" onclick="renderModal(event)" src="${room.images[i]}">
+        <div class="modal img-${i}">
+            <span class="close-btn">&times;</span>
+            <div class="modal-content row">
+                <img class="modal-img justify-content-center" src="${room.images[i]}">
+            </div>
+        </div>
+        `
+        containerImg.append(div);
+    }
+}
+
+/* Render Host */
+
+function renderHost(room) {
+    const host = document.getElementById('host-info');
+    let div = document.createElement('div');
+    div.innerHTML = `
+        <p class="text-secondary font-weight-bold">Chủ trọ:  ${room.owner.name}</p>
+        <p class="text-secondary font-weight-bold">SĐT:  ${room.owner.phoneNumber}</p>
+    `
+    host.append(div);
+}
+
+function renderNote(room) {
+    const note = document.getElementById('room-note');
+    note.innerHTML = `
+        ${room.details.note.replace(/\n/gi, '<br>')}
+    `
+}
+
+/* Switch home/room page */
+
+function homeToRoom() {
+    homePage.style.display = 'none';
+    roomPage.style.display = 'block';
+}
+
+function roomToHome() {
+    homePage.style.display = 'block';
+    roomPage.style.display = 'none';
 }
